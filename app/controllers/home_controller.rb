4c53d11a95bc3220b99e33456b345698910f0d18
Laura Purcell <mrs.laura.purcell@gmail.com>
2013-07-07 18:03:03 -0500

    Testing Unslider

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   # To change this template use File | Settings | File Templates.
   def index
-    render :layout => 'application_index2'
+    render :layout => 'application_plain'
   end
 end
