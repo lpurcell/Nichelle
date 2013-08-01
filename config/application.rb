9c427349a8d6d7a07c2d05c3ae79ca38c143aa6e
Laura Purcell <mrs.laura.purcell@gmail.com>
2013-07-31 22:55:14 -0500

    css changes

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,9 @@ module Niche
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.precompile += %w(.svg .eot .woff .ttf)
+
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
